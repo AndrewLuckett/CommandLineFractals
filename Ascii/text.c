ad89b3b1c4792d771cc6a4b60e8030aa8629e4b7
Andrew Luckett <andyluckett9@gmail.com>
2018-10-13 23:30:07 +0100

    Initial Commit

---
+++ Ascii/text.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct complexnumber {
+	double real;
+	double imaginary;
+} complex;
+
+typedef unsigned int uint;
+
+#ifndef max
+	#define max( a, b ) ( ((a) > (b)) ? (a) : (b) )
+#endif
+
+#ifndef min
+	#define min( a, b ) ( ((a) < (b)) ? (a) : (b) )
+#endif
+
+uint fractalAlgo(complex z, complex c, uint cutoffDepth) {
+	uint steps = 0;
+	for (uint i = 0; i < cutoffDepth; i++) {
+		steps++;
+		double r = (z.real * z.real) - (z.imaginary * z.imaginary) + c.real;
+		double i = (2 * z.real*z.imaginary) + c.imaginary;
+
+		if (r*r + i * i > 4) {
+			return steps;
+		}
+
+		z.real = r;
+		z.imaginary = i;
+	}
+	return 0; //Is in set
+}
+
+
+void printline(char *lineDat, int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%c", lineDat[i]);
+	}
+	printf("\n");
+}
+
+int main(int argc, char** argv) {
+	/*
+		d(x,y) : output columns and rows
+		c(x,y) : coordinate of centre of drawn fractal
+		z(x) : zoom value for drawn fractal
+		j(x,y) : julia values (optional, added to instead draw a julia set)
+	*/
+
+	int outDimensions[2] = { 60,30 }; //x,y
+	double centre[2] = { 0,0 }; //x,y
+	double zoom = 1;
+	double julia[2];
+
+	for (int i = 0; i < argc; i++) { //set arg values
+		printf("%s \n", argv[i]);
+	}
+
+	int mathDimension = min(outDimensions[0], outDimensions[1]);
+
+	double pixelwidth = 2 / (mathDimension*zoom); //4x supersample
+
+	for (int y = -outDimensions[1]; y < outDimensions[1]; y+=2) {
+		char *linedat = malloc(outDimensions[0]);
+
+		for (int x = -outDimensions[0]; x < outDimensions[0]; x+=2) { //loop for each true 'pixel'
+			char out = 65; //set to 0 when added lookup table
+
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < 2; j++) { //loop for super sample
+					complex inZ = {
+						pixelwidth*(x + j) + centre[0],
+						pixelwidth*(y + i) + centre[1]
+					};
+					complex inC = { 0,0 };
+					if (fractalAlgo(inC,inZ,1024) == 0) { //fixed cutoff
+						out = out + (char) pow(2, 2 * i + j);
+					}					
+				}
+			}
+			linedat[(x + outDimensions[0])/2] = out;
+		}
+		printline(linedat,outDimensions[0]);
+	}
+}
+
+
+
+
+
+
+
+
